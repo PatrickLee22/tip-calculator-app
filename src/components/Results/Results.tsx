409a477c8396a4dea376bcf105bccfefd39923c5
PatrickLee22 <patmlee22@gmail.com>
2023-06-28 17:02:53 -0500

    Implemented certain divs into Components

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -28,8 +28,8 @@ const Results = ({values, setValues}:Props) => {
                 <div className='tip__amount'>
                     <p>${values.tipAmount}</p>
                 </div>
-          </div>
-          <div className='total-result'>
+            </div>
+            <div className='total-result'>
                 <div className='total__label'>
                     <p className='amount-label'>Total</p>
                     <p className='person-label'>/person</p>
@@ -37,12 +37,14 @@ const Results = ({values, setValues}:Props) => {
                 <div className='total__amount'>
                     <p>${values.total}</p>
                 </div>
-          </div>
+            </div>
 
-          <button 
-            className='resetButton'
-            onClick={(e) => resetState()}     
-        >Reset</button>
+            <button 
+                className='resetButton'
+                onClick={(e) => resetState()}     
+            >
+                Reset
+            </button>
         </section>
     )
 }
